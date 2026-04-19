d2b88b961a04e62e4c53b3ad19ff53e67d4c55f9
leo <leo250105but@gmail.com>
2026-04-19 00:45:44 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'luna-cardapio-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna o cache se achar, senão baixa da rede
+        return response || fetch(event.request);
+      })
+  );
+});
